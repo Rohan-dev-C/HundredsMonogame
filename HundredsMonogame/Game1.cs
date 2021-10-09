bb3adccbd8641b4da0cf4d30dafc0819e7394908
Rohan Sampath <Rohan.Sampath@GMR.local>
2021-10-09 14:29:31 -0700

    Add project files.

---
+++ HundredsMonogame/Game1.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using System.Collections.Generic;
+
+namespace HundredsMonogame
+{
+
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        int levelNumber = 0;
+
+        
+
+        Texture2D ballTexture; 
+
+        List<Ball> balls = new List<Ball>();
+
+        SpriteFont font;
+
+        bool GameStarted = false;
+
+        bool isPressed;
+
+        bool ballSelected; 
+
+
+        void newLevel(ref int levelNum, ref List<Ball> ball)
+        {
+            ball.Clear();
+            levelNum++;
+            for(int i = 0; i < levelNum; i++)
+            {
+                ball.Add(new Ball(new Vector2(100 * i + 5, 100 * i + 5), ballTexture, Color.Black, new Vector2(0.3f, 0.3f), 0, new Vector2(5,4), false));
+            }
+        }
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            graphics.PreferredBackBufferWidth = 1000;
+            graphics.PreferredBackBufferHeight = 1000;
+            graphics.ApplyChanges();
+            base.Initialize();
+
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            ballTexture = Content.Load<Texture2D>("whiteCircle");
+            font = Content.Load<SpriteFont>("font");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            KeyboardState ks = Keyboard.GetState();
+
+            if (GameStarted == false && ks.IsKeyDown(Keys.Space))
+            {
+                GameStarted = true;
+                levelNumber = 1; 
+                newLevel(ref levelNumber, ref balls);
+            }
+
+            for (int i = 0; i < balls.Count; i++)
+            {
+                balls[i].UpdateBall(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+            }
+
+            if(ks.IsKeyDown(Keys.LeftShift))
+            {
+                isPressed = true; 
+            }
+            else if(!ks.IsKeyDown(Keys.LeftShift))
+            {
+                isPressed = false; 
+            }
+
+
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.DarkSlateGray);
+            spriteBatch.Begin();
+            // TODO: Add your drawing code here
+
+            if (GameStarted == false)
+            {
+                spriteBatch.DrawString(font, "Press Space to Start", new Vector2(300, 300), Color.Black);
+                spriteBatch.DrawString(font, "Click on a Circle in Game to Select", new Vector2(300, 200), Color.Black);
+            }
+            else if (GameStarted)
+            {
+                for (int i = 0; i < balls.Count; i++)
+                {
+                    balls[i].Draw(spriteBatch);
+                }
+                if(ballSelected == false)
+                {
+                    spriteBatch.DrawString(font, "Please Select a Ball", new Vector2(300, 50), Color.Black);
+                }
+            }
+
+            spriteBatch.End(); 
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ HundredsMonogame/ball.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+
+namespace HundredsMonogame
+{
+    class Ball : Sprite
+    {
+        int value;
+        public Vector2 speed;
+        public bool Selected; 
+        public Ball(Vector2 position, Texture2D texture, Color color, Vector2 Scale, int value, Vector2 speed, bool selected) : base(position, texture, color, Scale)
+        {
+            this.position = position;
+            this.texture = texture; 
+            this.color = color; 
+            this.scale = Scale;
+            this.value = value; 
+            this.speed = speed;
+            this.Selected = selected; 
+        }
+
+        public void UpdateBall(int screenWidth, int screenHeight)
+        {
+            position.X += speed.X;
+            position.Y += speed.Y;
+
+            if (position.Y < 0)
+            {
+                speed.Y = Math.Abs(speed.Y);
+            }
+            if (position.Y + texture.Height * scale.Y > screenHeight)
+            {
+                speed.Y = -speed.Y;
+            }
+            if(position.X < 0)
+            {
+                speed.X = Math.Abs(speed.X); 
+            }
+            if (position.X + texture.Width * scale.X > screenWidth)
+            {
+                speed.X = -speed.X; 
+            }
+       
+        }
+
+    }
+}
